efd142964508e455262d4c7e799c23a8893018ab
JCoMon <jericholance.monsalud.iics@ust.edu.ph>
2020-02-15 16:44:14 +0800

    first commit

---
+++ js/form.js
@@ -0,0 +1,12 @@
+ function validate(){
+    var Events = document.getElementById("Events");
+    
+    if (Events.value ==""){
+        alert("Select an option!");
+        return true;
+        
+    }
+        alert("Your information has been saved!")
+    return false;
+    
+}   
